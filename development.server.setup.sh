dc14a124184894155e2200832ac827ecaf39b349
Bagus Mahendra Wicaksono <46114453+bagusmahendrawicaksono@users.noreply.github.com>
2024-03-08 10:18:47 +0800

    Update development.server.setup.sh

--- development.server.setup.sh
+++ development.server.setup.sh
@@ -2,4 +2,32 @@
 apt update  # To get the latest package lists
 apt install openjdk-21-jdk -y
 apt install nodejs -y
-sudo apt install npm -y
+apt install npm -y
+#Install Docker
+echo -n "Install Docker\n"
+apt install ca-certificates curl gnupg
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update
+apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+gpasswd -a $USER docker
+newgrp docker
+#Install Minikube
+echo -n "Install Minikube\n"
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
+dpkg -i minikube_latest_amd64.deb
+#Install kubectl
+echo -n "Install Kubectl\n"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+#Install Skaffold
+curl -Lo skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64 && \
+install skaffold /usr/local/bin/
+
+
+
